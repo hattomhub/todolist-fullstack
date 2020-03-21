e4a90e29d0af30d683e54a4645c1f71a083c80bb
joao-madeira <joaomadeira@outlook.pt>
2020-03-21 15:43:09 +0000

    Client side finished and working perfectly with server side

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -3,17 +3,17 @@ import React, { Fragment, useState } from "react";
 const InputTodo = () => {
     const [description, setDescription] = useState("");
 
-    const onSubmitForm = async e => {
+    const onSubmitForm = async (e) => {
         try {
             e.preventDefault();
-            const body = {description};
-            const response = fetch("http://localhost:5000/todos", {
+            const body = { description };
+            const response = await fetch("http://localhost:5000/todos", {
                 method: "POST",
-                headers: {'Content-Type': 'application/json'},
+                headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(body)
             });
 
-            console.log (response);
+            window.location = "/";
         } catch (error) {
             console.log(error.message);
         }
